aa9a6fe586d868f0f3547b83db0adacecba2894a
HBB20 <harshbbhakta@gmail.com>
2016-12-25 12:20:14 +0530

    adds files from existing project

---
+++ codegenerator.js
@@ -0,0 +1,392 @@
+var textAreaInput, textAreaOutput;
+var views;
+var initialized = false;
+var outputType = 3; //1: Activity 2: Fragment  3: ButterKnife8 4: ButterKnife
+var liActivities, liFragments, liButterKnife, liButterKnife8, lis;
+/*This will set initial value for input and set output for the same. 
+It sets event listener to inputArea so that on every change in inputText will regenerate output.
+Then it focuses on inputArea so user can directly paste it with out additional click*/
+function init() {
+    textAreaInput = document.getElementById("textAreaInput");
+    textAreaOutput = document.getElementById("textAreaOutput");
+    liActivities = document.getElementById("li-activity");
+    liFragments = document.getElementById("li-fragment");
+    liButterKnife = document.getElementById("li-butterknife");
+    liButterKnife8 = document.getElementById("li-butterknife8");
+    textAreaInput.value = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<RelativeLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n    xmlns:tools=\"http://schemas.android.com/tools\"\n    android:layout_width=\"match_parent\"\n    android:layout_height=\"match_parent\">\n\n    <LinearLayout\n        android:id=\"@+id/linear_parent\"\n        android:layout_width=\"match_parent\"\n        android:layout_height=\"match_parent\"\n        android:orientation=\"vertical\">\n\n       <EditText\n                    android:id=\"@+id/edittext_userName\"\n                    android:layout_width=\"match_parent\"\n                    android:layout_height=\"wrap_content\"/>\n\n        <EditText\n                    android:id=\"@+id/edittext_password\"\n                    android:layout_width=\"match_parent\"\n                    android:layout_height=\"wrap_content\"/>\n\n        <Button\n                android:id=\"@+id/button_login\"\n                android:layout_width=\"match_parent\"\n                android:layout_height=\"wrap_content\"/>\n</RelativeLayout>\n";
+    textAreaInput.addEventListener("input", updateOutput, false);
+
+    lis = new Array();
+    lis.push(liActivities);
+    lis.push(liFragments);
+    lis.push(liButterKnife8);
+    lis.push(liButterKnife);
+    update_output_type(3);
+    textAreaInput.focus();
+}
+
+function update_output_type(typeID) {
+    outputType = typeID;
+    for (var i = 0; i < lis.length; i++) {
+        if (i + 1 == typeID) { //active type
+            lis[i].className = "li-custom active";
+        } else {
+            lis[i].className = "li-custom";
+        }
+
+    };
+    updateOutput();
+}
+
+/*This will process on inputText and puts according output in output area.
+ */
+function updateOutput() {
+    // initialize views array
+    views = new Array();
+
+    // checks inputText and fill views array
+    readAllViews();
+
+
+    // final output variable
+    var finalResult = "";
+
+    if (outputType == 1) {
+        finalResult = getOutputForActivity();
+    } else if (outputType == 2) {
+        finalResult = getOutputForFragment();
+    } else if (outputType == 3) {
+        finalResult = getOutputForButterKnife8();
+    } else if (outputType == 4) {
+        finalResult = getOutputForButterKnife();
+    }
+
+    console.log("output is " + finalResult);
+
+
+    //analytics
+    if (initialized) {
+        //ga('send', 'event', 'codeGeneration', 'outPut updated', finalResult);
+    }
+    //sets final result to output
+    textAreaOutput.value = finalResult;
+
+    //sets foucus on output and select entire text of output. Now developer is only cltr + c press ways from copy the code.
+    textAreaOutput.focus();
+
+    //set flag
+    initialized = true;
+}
+
+
+function getOutputForButterKnife() {
+    var finalResult = "";
+    console.log("I'm in function butterknife");
+    //when atleast 1 view with android:id is found, appends output of each single view to finalResult
+    if (views.length != 0) {
+        finalResult = "/** ButterKnife Code **/";
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var singleOutPut = getOutputLineForButterKnifeView(view);
+            finalResult = finalResult + singleOutPut;
+        }
+        finalResult = finalResult + "\n /** ButterKnife Code **/"
+    } else {
+        finalResult = "/**No view found**/ ";
+    }
+    return finalResult;
+}
+
+function getOutputForButterKnife8() {
+    var finalResult = "";
+    console.log("I'm in function butterknife");
+    //when atleast 1 view with android:id is found, appends output of each single view to finalResult
+    if (views.length != 0) {
+        finalResult = "/** ButterKnife Code **/";
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var singleOutPut = getOutputLineForButterKnife8View(view);
+            finalResult = finalResult + singleOutPut;
+        }
+        finalResult = finalResult + "\n /** ButterKnife Code **/"
+    } else {
+        finalResult = "/**No view found**/ ";
+    }
+    return finalResult;
+}
+
+function getOutputForActivity() {
+    var finalResult = "Activity Output";
+    var viewDeclaration = getViewDeclaration();
+    var activityBinder = getActivityBinderCode();
+    return viewDeclaration + "\n\n" + activityBinder;
+}
+
+function getOutputForFragment() {
+    var finalResult = "Fragment output";
+    var viewDeclaration = getViewDeclaration();
+    var fragmentBinder = getFragmentBinderCode();
+    return viewDeclaration + "\n\n" + fragmentBinder;
+}
+
+function getViewDeclaration() {
+    var finalResult = "";
+    if (views.length != 0) {
+        finalResult = "/** Android Views **/";
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var singleOutPut = getOutputLineForViewDeclaration(view);
+            finalResult = finalResult + singleOutPut;
+        }
+        finalResult = finalResult + "\n /** Android Views **/"
+    } else {
+        finalResult = "/**No view found**/ ";
+    }
+    return finalResult;
+}
+
+function getActivityBinderCode() {
+    var finalResult = "";
+    if (views.length != 0) {
+        finalResult = "/**\n * Binds XML views \n * Call this function after setContentView() in onCreate().\n**/ ";
+        finalResult = finalResult + " \n private void bindViews(){"
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var singleOutPut = getOutputLineForActivityBind(view);
+            finalResult = finalResult + "\n" + singleOutPut;
+        }
+        finalResult = finalResult + "\n}"
+    }
+    return finalResult;
+}
+
+function getFragmentBinderCode() {
+    var finalResult = "";
+    if (views.length != 0) {
+        finalResult = "/**\n * Binds XML views \n * Call this function after layout is ready.\n**/ ";
+        finalResult = finalResult + " \n private void bindViews(View rootView){"
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var singleOutPut = getOutputLineForFragmentBind(view);
+            finalResult = finalResult + "\n" + singleOutPut;
+        }
+        finalResult = finalResult + "\n}"
+    }
+    return finalResult;
+}
+
+/*This will find each and every view from input text
+  if the view has 'android:id=' then that view will be added to views*/
+function readAllViews() {
+    var xmlText = textAreaInput.value;
+    var lastViewEnd = -1;
+    while (lastViewEnd <= xmlText.length) {
+        if (xmlText.indexOf("<", lastViewEnd + 1) > -1) {
+            var viewBeginIndex = xmlText.indexOf("<", lastViewEnd + 1);
+            var viewEndIndex = xmlText.indexOf(">", viewBeginIndex);
+            if (viewBeginIndex > -1 && viewEndIndex > -1) {
+                var view = xmlText.substring(viewBeginIndex, viewEndIndex + 1);
+                if (isEligibleView(view)) {
+                    views.push(view);
+                }
+                lastViewEnd = viewEndIndex;
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+
+/*If view has "android:id" and className and id are not of length 0 then retun true or false otherwise.*/
+function isEligibleView(view) {
+    if (view.indexOf("android:id=") > -1) {
+        if (getClassNameForView(view).trim().length > 0 && getIdFromView(view).trim().length > 0) {
+            console.log("Eligibility: true=>" + view);
+            return true;
+        } else {
+            console.log("Eligibility: false=>" + view);
+            return false;
+        }
+    } else {
+        console.log("Eligibility noID: false=>" + view);
+        return false;
+    }
+}
+
+
+/*
+Returns output for single view.
+this is sample oneViewXml
+  <Button
+    android:id="@+id/loginButton"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"/>
+  
+  output will be=>
+    @Bind(R.id.loginButton) 
+    Button loginButton;
+
+*/
+function getOutputLineForButterKnifeView(oneViewXml) {
+    // ga('send', 'event', 'Process', 'singleOutPut request', "");
+    return "\n@Bind(R.id." + getIdFromView(oneViewXml).trim() + ") \n" + getClassNameForView(oneViewXml).trim() + " " + getJavaNameForView(oneViewXml).trim() + ";";
+}
+
+
+/*
+Returns output for single view.
+this is sample oneViewXml
+  <Button
+    android:id="@+id/loginButton"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"/>
+  
+  output will be=>
+    @BindView(R.id.loginButton) 
+    Button loginButton;
+
+*/
+function getOutputLineForButterKnife8View(oneViewXml) {
+    // ga('send', 'event', 'Process', 'singleOutPut request', "");
+    return "\n@BindView(R.id." + getIdFromView(oneViewXml).trim() + ") \n" + getClassNameForView(oneViewXml).trim() + " " + getJavaNameForView(oneViewXml).trim() + ";";
+}
+
+/*
+Returns output for single view.
+this is sample oneViewXml
+  <Button
+    android:id="@+id/loginButton"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"/>
+  
+  output will be=>
+    Button loginButton;
+
+*/
+function getOutputLineForViewDeclaration(oneViewXml) {
+    // ga('send', 'event', 'Process', 'singleOutPut request', "");
+    return "\n" + getClassNameForView(oneViewXml).trim() + " " + getJavaNameForView(oneViewXml).trim() + ";";
+}
+
+/*
+Returns output for single view.
+this is sample oneViewXml
+  <Button
+    android:id="@+id/login_button"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"/>
+  
+  output will be=>
+    loginButton = (Button)findViewById(R.id.login_button);
+
+*/
+function getOutputLineForActivityBind(oneViewXml) {
+    // ga('send', 'event', 'Process', 'singleOutPut request', "");
+    return "\t " + getJavaNameForView(oneViewXml).trim() + " = (" + getClassNameForView(oneViewXml).trim() + ") findViewById(R.id." + getIdFromView(oneViewXml).trim() + ");";
+}
+
+/*
+Returns output for single view.
+this is sample oneViewXml
+  <Button
+    android:id="@+id/login_button"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"/>
+  
+  output will be=>
+    loginButton = (Button)rootView.findViewById(R.id.login_button);
+
+*/
+function getOutputLineForFragmentBind(oneViewXml) {
+    // ga('send', 'event', 'Process', 'singleOutPut request', "");
+    return "\t " + getJavaNameForView(oneViewXml).trim() + " = (" + getClassNameForView(oneViewXml).trim() + ") rootView.findViewById(R.id." + getIdFromView(oneViewXml).trim() + ");";
+}
+
+
+/*this will return class name from xml view
+sample view
+<Button
+  android:id="@+id/loginButton"
+  android:layout_width="match_parent"
+  android:layout_height="wrap_content"/>
+
+returns=>
+"Button"
+
+*/
+function getClassNameForView(oneViewXml) {
+    var output;
+    var firstAngularIndex = oneViewXml.indexOf("<");
+    if (firstAngularIndex > -1) {
+        var firstSpaceIndex = oneViewXml.indexOf(" ", firstAngularIndex);
+        output = oneViewXml.substring(firstAngularIndex + 1, firstSpaceIndex);
+        return output;
+    }
+    return "Object";
+}
+
+
+/*this will return ID from xml view
+sample view
+<Button
+  android:id="@+id/login_button"
+  android:layout_width="match_parent"
+  android:layout_height="wrap_content"/>
+
+returns=>
+"login_button"
+*/
+function getIdFromView(oneViewXml) {
+    if (oneViewXml.indexOf("android:id=") > -1) {
+        var lineBegin = oneViewXml.indexOf("android:id=");
+        var lineFirstComma = oneViewXml.indexOf('/', lineBegin);
+        var lineLastComma = oneViewXml.indexOf('"', lineFirstComma + 1);
+        var outPut = lineBegin + ":linebegin  " + lineFirstComma + ":lineFirstComma  " + lineLastComma + ":lineLastComma";
+        outPut = oneViewXml.substring(lineFirstComma + 1, lineLastComma);
+        return outPut;
+    } else {
+        return "";
+    }
+}
+
+/*this will return javaname from xml view
+sample view
+<Button
+  android:id="@+id/login_button"
+  android:layout_width="match_parent"
+  android:layout_height="wrap_content"/>
+
+returns=>
+"loginButton"
+*/
+function getJavaNameForView(oneViewXml) {
+    var xmlId = getIdFromView(oneViewXml).trim();
+
+    var javaName = "";
+    var underScoreIndex = xmlId.indexOf("_");
+
+    if (underScoreIndex == -1) { //no '_' in id then use id as name
+        javaName = xmlId;
+    } else {
+        var tokens = xmlId.split("_");
+        for (var i = 0; i < tokens.length; i++) {
+            var token = tokens[i].trim();
+            if (token.length > 0) {
+                var firstChar = token.charAt(0);
+                var capToken = "";
+                if (javaName.length == 0) {
+                    capToken = token;
+                } else {
+                    capToken = firstChar.toUpperCase() + token.slice(1);
+                }
+
+                javaName = javaName + capToken;
+            }
+        }
+    }
+    // ga('send', 'event', 'Process', 'javaname generation', "xmlId:" + xmlId + "; javaName:" + javaName);
+    return javaName;
+}
